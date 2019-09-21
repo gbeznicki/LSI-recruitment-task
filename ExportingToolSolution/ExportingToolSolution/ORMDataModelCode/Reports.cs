832142a61800b9e144ef091c2d128d164758aee8
gbeznicki <g.beznicki@gmail.com>
2019-09-21 18:28:55 +0200

    New project, finally working!!!

---
+++ ExportingToolSolution/ExportingToolSolution/ORMDataModelCode/Reports.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using DevExpress.Data.Filtering;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+namespace ExportingToolSolution.Database
+{
+
+    public partial class Reports
+    {
+        public Reports(Session session) : base(session) { }
+        public override void AfterConstruction() { base.AfterConstruction(); }
+    }
+
+}
